5f8263ca9e9d10fe1697c2af8fa0988668741504
sufei <sufei@weichengya.cn>
2026-07-02 17:23:28 +0800

    ui

---
+++ my_container.gd
@@ -0,0 +1,12 @@
+extends Container
+
+func _notification(what):
+	if what == NOTIFICATION_SORT_CHILDREN:
+		# Must re-sort the children
+		for c in get_children():
+			# Fit to own size
+			fit_child_in_rect(c, Rect2(Vector2(), size))
+
+func set_some_setting():
+	# Some setting changed, ask for children re-sort.
+	queue_sort()
